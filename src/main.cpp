9d1fba5f7e00fbb3434ee209f8488856cd62213f
Amonynuss <j.malende@gmx.de>
2025-11-20 13:58:46 +0100

    Added booting output in terminal

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <ampel.h>
 #include <sensor.h>
 #include <mqtt_sender.h>
+#include "startup.h"
 
+Startup startup;
 Ampel ampel;
 Sensor sensor;
 MqttSender mqttSender;
 
 void setup() {  
   Serial.begin(9600);
+  while(!Serial){}
+  startup.printBootArt();
   ampel.initialize();
   ampel.setBrightness(200);
   ampel.setRed();
